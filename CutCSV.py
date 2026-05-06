55ccc87eb212b27eb878c16d0ae52c460bfc1873
Chang Xu <45734103+5260486@users.noreply.github.com>
2026-05-06 14:29:13 +0800

    Add files via upload

--- CutCSV.py
+++ CutCSV.py
@@ -11,8 +11,14 @@ csvfiles = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 Filename.Filename.write_filenames(folder_path,csvfiles)
 csvnames =Filename.Filename.read_filenames(folder_path,'filenames.txt')
 
-head_cut=1
+# 根据数据文件显示的采集分辨率及需裁剪的时间长度，得到裁剪的行数
+stepsize=0.001
+time=6
+rownum=time/stepsize+1
+
+head_cut=rownum
 tail_cut=0
+
 for idx,filename in enumerate(csvfiles,1):
     input_file = os.path.join(folder_path, filename)
     output_file = os.path.join(output_folder_path, filename)
